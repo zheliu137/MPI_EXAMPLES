1843fdb0546885c88cef335609e8fedca4df135e
zheliu137 <zheliu137@gmail.com>
2023-09-06 18:06:34 +0800

    Allgatherv Vs setzero + Sum

--- Perf/AllgatherVsSum/main.f90
+++ Perf/AllgatherVsSum/main.f90
@@ -1,38 +1,71 @@
 program main
-implicit none
     USE parallel_include
     USE mp_global
     USE mp
     USE mp_world,  ONLY : nproc, myid=>mpime, world_comm
     implicit none
-integer :: myid, ierr
-integer :: npp=10
-integer :: i, j, k, l
-integer,allocatable :: ifull(:)
-integer,allocatable:: np(:), offset(:)
+    integer :: ierr
+    integer :: npp=100000,nloop=10000
+    integer :: i, j, k, l
+    integer,allocatable :: ifull(:)
+    integer,allocatable:: np(:), offset(:)
+    CALL mp_startup()
 
-CALL mp_startup()
+    ! write(*,"('myid = ',I4,' nproc : ',I4)"), myid, nproc
+    ALLOCATE(ifull(npp*nproc),&
+    np(nproc),offset(nproc))
 
-ALLOCATE(ifull(npp*nproc),&
-np(nproc),offset(nproc))
 
-ifull=myid
+    ! write(*,"('myid = ',I4,' start ifull : ',1000I4)"), myid, ifull
+    CALL MP_BARRIER(world_comm)
+    np=npp
+    do i = 1, nproc
+        offset(i) = (i-1) * npp
+    enddo
+    ! write(*,"('myid = ',I4,' offset : ',1000I4)"), myid, offset
+    do i = 1, nloop
+        CALL start_clock("allgatherv")
+        ifull(offset(myid+1)+1:offset(myid+1)+npp)=myid
+        CALL MPI_ALLGATHERV( MPI_IN_PLACE, 0, MPI_DATATYPE_NULL, &
+            ifull, np, offset, MPI_INTEGER, world_comm, ierr )
+        CALL stop_clock("allgatherv")
+    enddo
 
-CALL MPI_BARRIER(world_comm, ierr)
-write(*,"('myid = ',I4,' ifull : ',10I4)"), myid, ifull
-np=npp
-do i = 1 nproc
-    offset(i) = myid * npp
-enddo
+    CALL MP_BARRIER(world_comm)
+    ! check results
+    do i = 1, nproc
+        do j = 1,npp
+            if( ifull((i-1)*npp+j) .ne. i-1 ) write(*,*)"ERROR",myid,i,j
+        enddo
+    enddo
 
-CALL MPI_ALLGATHERV( MPI_IN_PLACE, 0, MPI_DATATYPE_NULL, &
-       ifull, np, offset, MPI_INTEGER, world_comm, ierr )
+    ! write(*,"('myid = ',I4,' ifull : ',1000I4)"), myid, ifull
 
-CALL MP_BARRIER(world_comm)
-write(*,"('myid = ',I4,' ifull : ',10I4)"), myid, ifull
+    CALL MP_BARRIER(world_comm)
 
-CALL MP_BARRIER(world_comm)
+    np=npp
+    do i = 1, nproc
+        offset(i) = (i-1) * npp
+    enddo
+    ! write(*,"('myid = ',I4,' offset : ',1000I4)"), myid, offset
+    CALL start_clock("MPSUM")
+    do i = 1, nloop
+        ifull=0
+        ifull(offset(myid+1)+1:offset(myid+1)+npp)=myid
+        CALL mp_sum(ifull,world_comm)
+    enddo
+    CALL stop_clock("MPSUM")
 
-CALL mp_global_end()
+    CALL MP_BARRIER(world_comm)
+    ! check results
+    do i = 1, nproc
+        do j = 1,npp
+            if( ifull((i-1)*npp+j) .ne. i-1 ) write(*,*)"ERROR",myid,i,j
+        enddo
+    enddo
+
+    if(myid==0)CALL print_clock("allgatherv")
+    if(myid==0)CALL print_clock("MPSUM")
+    CALL mp_global_end()
 
 end program
